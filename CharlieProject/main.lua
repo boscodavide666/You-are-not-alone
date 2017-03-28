8b346c294a85d520ea8c584ace5cb90ff016655a
Davide Bosco <boscodavide25@gmail.com>
2017-03-28 10:06:55 +0200

    add idle e piccole sistemazioni

--- CharlieProject/main.lua
+++ CharlieProject/main.lua
@@ -3,21 +3,15 @@ local player =  require "modules/player"
 
 function love.load(arg)
 player.load()
-scale = 3
-circleTransition = love.graphics.newImage("assets/Image/transition4.png")
 end
 
 function love.update(dt)
 background.move(player.vel, player.location, player.posY)
---while scale > 0 do
-scale = scale - dt
---end
+player.animation(dt)
 end
 
 function love.draw()
 love.graphics.draw(background.startBg, background.posX, background.posY)
 love.graphics.draw(player.img, player.posX, player.posY)
-
-love.graphics.draw(circleTransition, love.graphics.getWidth()/2, love.graphics.getHeight()/2, 0, scale, scale, love.graphics.getWidth()/2, love.graphics.getHeight()/2)
 
 end
--- CharlieProject/modules/player.lua
+++ CharlieProject/modules/player.lua
@@ -8,14 +8,33 @@ player.height = 0
 player.vel = 0
 player.location = 0
 
+player.timer = 0
+player.frameDuration = 0.4
+
+playerAnimation =  {
+  img01 = love.graphics.newImage("assets/Character/statico1.png"),
+  img02 = love.graphics.newImage("assets/Character/statico2.png")
+}
+
 function player.load()
-player.img = love.graphics.newImage("assets/Character/statico1.png")
+player.img = playerAnimation.img01
 player.posX = love.graphics.getWidth()/2
 player.posY = love.graphics.getHeight()/2
-player.vel = 10
+player.vel = 5
 end
 
+function player.animation(int)
+player.timer = player.timer + int
 
+if (player.timer >= player.frameDuration) then
+  if (player.img == playerAnimation.img01) then
+    player.img = playerAnimation.img02
+  else
+    player.img = playerAnimation.img01
+  end
+  player.timer = 0
+end
+end
 
 
 return player
