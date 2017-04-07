78c1ad8df15f3c10eef269df1382a544190d3759
Davide Bosco <boscodavide25@gmail.com>
2017-04-07 17:20:19 +0200

    rimosso bugghino suono

--- CharlieProject/modules/Clues/clue01.lua
+++ CharlieProject/modules/Clues/clue01.lua
@@ -28,7 +28,6 @@ end
 
 function clue01.update(dt, _x, _y)
 if (_x >= clue01Sprite.x - 20 and _x <= clue01Sprite.x + clue01.size) and (_y >= clue01Sprite.y and _y <= clue01Sprite.y + clue01.size ) then
-sound:play()
 clue01.isOver = true
 clue01.setRead()
 else
