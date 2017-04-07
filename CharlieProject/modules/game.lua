f091296fa54d0dbf98111af449e3c0bb7bffe8e2
Davide Bosco <boscodavide25@gmail.com>
2017-04-07 16:45:28 +0200

    sistemaz minime

--- CharlieProject/modules/game.lua
+++ CharlieProject/modules/game.lua
@@ -69,7 +69,7 @@ end
 end
 
 function game.dialogue(int)
-local dialogues = {"Evviva il guerriero della luce e' arrivato!!\nVai a parlare con il capovillaggio ti sta aspettando!", "Benvenuto nel nostro villaggio guerriero della luce!\nLa situazione e' sempre piu' drammatica per colpa di quei dannati mostri che infestano la caverna a nord!\nOgni notte mietono nuove vittime!\nTi prego aiutaci!",
+local dialogues = {"Evviva il guerriero della luce e' arrivato!!\nVai a parlare con il capovillaggio ti sta aspettando!", "Benvenuto nel nostro villaggio guerriero della luce!\nSono il capovillaggio!\nLa situazione e' sempre piu' drammatica per colpa di quei dannati mostri che infestano la caverna a nord!\nOgni notte mietono nuove vittime!\nTi prego aiutaci!",
 "La mia famiglia sta facendo i bagagli per scappare!\nAiutaci!","Sarebbe un villaggio cosi' bello se non fosse per i mostri", "Prima di entrare nel dungeon parla con il capovillaggio!", "Buona fortuna avventuriero!"}
 
 return dialogues[int]
